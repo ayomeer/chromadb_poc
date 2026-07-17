df17b9730d66f2858b3783693fd7f0643fd288d9
Andreas <ayomeer@gmail.com>
2026-07-17 07:52:58 +0000

    adding cluster introspection plottin

--- src/chromadb_demo.py
+++ src/chromadb_demo.py
@@ -6,15 +6,22 @@ import chromadb
 from chromadb.api.types import EmbeddingFunction
 from chromadb.utils.embedding_functions.ollama_embedding_function import OllamaEmbeddingFunction
 
+import umap 
 
+# -- Functions -------------------------------------------------------------------------
 def add_data_to_collection(
     collection: chromadb.Collection, 
     data_path: str | Path
 ) -> None:
+    """Get chunks from json and load into collection (takes some minutes).
+
+    Args:
+        collection (chromadb.Collection): Collection to load data into.
+        data_path (str | Path): Path to json to load data from.
+    """
+
     #parse args
     data_path = Path(data_path)
-
-    
 
     # load collection from json and create chroma collection from it
     with open(data_path, "r") as file:
@@ -39,6 +46,38 @@ def add_data_to_collection(
     )
     return
 
+def text_query(
+    query_texts: list[str],
+    collection: chromadb.Collection,
+    n_results: int
+) -> None:
+    """_summary_
+
+    Args:
+        query_texts (list[str]): List of query strings.
+        collection (chromadb.Collection): Collection to query.
+        n_results (int): Number of results to show.
+    """
+    results = collection.query(
+        query_texts=query_texts,
+        n_results=n_results
+    )
+    
+    for i, query_text in enumerate(query_texts):
+        print(f"\nQuery results for query {i}: '{query_text}':")
+        for j in range(n_results):
+            print("----------------------------------------------------")
+            print(f"RESULT {j+1}:")
+            print("----------------------------------------------------")
+            print("metadatas\n", results["metadatas"][i][j])
+            print("doucuments:\n", results["documents"][i][j])
+            print("istances:\n", results["distances"][i][j])
+
+# -- Script Flow Control ---------------------------------------------------------------
+
+LOAD_NEW_DATA = False
+EXECUTION_MODE = 'query' # 'plot' or 'query'
+
 if __name__ == "__main__":
 
     # Set up chromadb objects
@@ -56,52 +95,36 @@ if __name__ == "__main__":
         embedding_function=ollama_ef,
     )
 
+    if LOAD_NEW_DATA:
+        add_data_to_collection(
+            collection=collection,
+            data_path="/proj/output/chroma_data.json"
+        )
 
-    # load collection from json and create chroma collection from it
-    # data_path="/proj/output/chroma_data.json"
-    # with open(data_path, "r") as file:
-    #     collection_data: list[dict] = json.load(file)
+    match EXECUTION_MODE:
+        case 'query':
+            text_query(
+                collection=collection,
+                n_results=5,
+                query_texts=["Gefahr für Amphibien beim überqueren von Strassen"]
+            )
 
-    # # flatten pdf doc data elements into lists of same lenth as chunks 
-    # ids = []
-    # documents = []
-    # metadatas = []
-    # i: int = 0
-    # for element in collection_data:
-    #     documents.extend(element["chunks"])
-    #     for _ in element["chunks"]:
-    #         i += 1
-    #         ids.append(str(i))
-    #         metadatas.append(element["metadata"])
+        case 'plot':
+            # reduce dimensionality w/ UMAP
+            reducer = umap.UMAP(
+                n_neighbors=15,
+                n_components=2,
+                metric='cosine'
+            )
 
-    # print("adding data to collection.")
-    # collection.add(
-    #     ids=ids,
-    #     documents=documents,
-    #     metadatas=metadatas
-    # )
+            results = collection.get(include=['embeddings'])
+            points = reducer.fit_transform(results["embeddings"])
+            dummy = 1
+            # plot 2D space
 
-    # query chromadb
-    n_results=5
-    query_texts=[
-        "Gefahr für Amphibien beim überqueren von Strassen"
-    ]
-    results = collection.query(
-        query_texts=query_texts,
-        n_results=n_results
-    )
-    
-    for i, query_text in enumerate(query_texts):
-        print(f"\nQuery results for query {i}: '{query_text}':")
-        for j in range(n_results):
-            print("----------------------------------------------------")
-            print(f"RESULT {j+1}:")
-            print("----------------------------------------------------")
-            print("metadatas\n", results["metadatas"][i][j])
-            print("doucuments:\n", results["documents"][i][j])
-            print("istances:\n", results["distances"][i][j])
+        case _:
+            print("Invalid EXECUTION_MODE.")
+            
 
-    # print("Query 2 Results:")
-    # print(results["metadatas"][1])
-    # print(results["metadatas"][1])
-    # print(results["distances"][1])
+
+
